d15d0a4fe357ecbf8ae2ff736692f750cf0d755f
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-08 08:58:24 -0500

    FIRST COMMIT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"pages/contactlist", component: ContactListComponent},
+  {path:"pages/settings", component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { LeftSideBarComponent } from './template/left-side-bar/left-side-bar.component';
+import { RightSideBarComponent } from './template/right-side-bar/right-side-bar.component';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LeftSideBarComponent,
+    RightSideBarComponent,
+    ContactListComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
