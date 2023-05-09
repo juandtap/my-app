387dc95401aaa8a8b9b1bb90b264db31c01f97dd
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-08 22:35:58 -0500

    left side bar component terminado

--- src/app/template/left-side-bar/left-side-bar.component.ts
+++ src/app/template/left-side-bar/left-side-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-left-side-bar',
   templateUrl: './left-side-bar.component.html',
-  styleUrls: ['./left-side-bar.component.scss']
+  styleUrls: ['../../app.component.scss','./left-side-bar.component.scss']
 })
 export class LeftSideBarComponent {
 
