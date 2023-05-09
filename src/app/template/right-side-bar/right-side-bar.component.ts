47b388fd002cb955fccc83adf799e3be5674b986
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-05-08 22:54:26 -0500

    right side bar component terminado

--- src/app/template/right-side-bar/right-side-bar.component.ts
+++ src/app/template/right-side-bar/right-side-bar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-right-side-bar',
   templateUrl: './right-side-bar.component.html',
-  styleUrls: ['./right-side-bar.component.scss']
+  styleUrls: ['../../app.component.scss','./right-side-bar.component.scss']
 })
 export class RightSideBarComponent {
 
